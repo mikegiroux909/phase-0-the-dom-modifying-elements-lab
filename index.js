daa2c39beef24bd025e9e63a805c919f2314ef2e
mike giroux <michaelgiroux909@gmail.com>
2023-01-13 13:54:12 -0800

    commit tree 2362a69dd15221350de7a175dc55b1a32906ab59

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+//const main = document.createElement("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "mike is the champion";
